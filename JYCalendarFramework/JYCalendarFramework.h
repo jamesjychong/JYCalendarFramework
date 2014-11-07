d932ae79db8597c7dc9960cb458c72aa23f9c1da
James Chong <jamesjychong@gmail.com>
2014-11-07 16:10:15 +0900

    Initial Commit

---
+++ JYCalendarFramework/JYCalendarFramework.h
@@ -0,0 +1,19 @@
+//
+//  JYCalendarFramework.h
+//  JYCalendarFramework
+//
+//  Created by Jae Yoon Chong on 11/7/14.
+//  Copyright (c) 2014 jamesjychong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JYCalendarFramework.
+FOUNDATION_EXPORT double JYCalendarFrameworkVersionNumber;
+
+//! Project version string for JYCalendarFramework.
+FOUNDATION_EXPORT const unsigned char JYCalendarFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JYCalendarFramework/PublicHeader.h>
+
+
